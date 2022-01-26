3f68f066c2275889c7f73ad276c48822acd9e077
Fernando Rojo <frojo@sas.upenn.edu>
2022-01-25 23:09:35 -0500

    :memo: add ts docs
    ts docs

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -43,6 +43,11 @@ const sidebars = {
       ],
       label: 'Recipes',
     },
+    {
+      type: 'category',
+      items: ['typescript/navigation-options', 'typescript/next-replacements'],
+      label: 'TypeScript',
+    },
     'resources',
     'community',
   ],
