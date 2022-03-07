a241c069a0c59502210c2776967ee968fc0eb4cb
Fernando Rojo <frojo@sas.upenn.edu>
2022-03-07 16:06:08 -0500

    :sparkles: Add useParam, to replace useRoute() + useRouter().query

---
+++ src/params/index.tsx
@@ -0,0 +1,215 @@
+// From https://gist.github.com/nandorojo/052887f99bb61b54845474f324aa41cc
+
+import Router, { useRouter } from 'next/router'
+import { useCallback, useMemo, useRef, useState, useEffect } from 'react'
+import { Platform } from 'react-native'
+
+import { useRoute } from './use-route'
+
+function useStable<T>(value: T) {
+  const ref = useRef(value)
+  useEffect(() => {
+    ref.current = value
+  }, [value])
+
+  return ref
+}
+
+function useStableCallback<T extends (...args: any[]) => any>(
+  callback: T | undefined
+): T {
+  const callbackRef = useRef(callback)
+
+  useEffect(() => {
+    callbackRef.current = callback
+  })
+
+  // https://github.com/facebook/react/issues/19240
+  return useMemo(() => ((...args) => callbackRef.current?.(...args)) as T, [])
+}
+
+type Config<
+  Props extends Record<string, unknown>,
+  Required extends boolean,
+  ParsedType,
+  InitialValue
+> = (Required extends false
+  ? {
+      parse?: (value?: string | string[]) => ParsedType
+    }
+  : {
+      parse: (value?: string | string[]) => ParsedType
+    }) & {
+  stringify?: (value: ParsedType) => string
+  initial: InitialValue
+  paramsToClearOnSetState?: (keyof Props)[]
+}
+
+type Params<
+  Props extends Record<string, unknown> = Record<string, string>,
+  Name extends keyof Props = keyof Props,
+  NullableUnparsedParsedType extends Props[Name] | undefined =
+    | Props[Name]
+    | undefined,
+  ParseFunction extends
+    | undefined
+    | ((
+        value?: string | string[]
+      ) => NonNullable<NullableUnparsedParsedType>) = (
+    value?: string | string[]
+  ) => NonNullable<NullableUnparsedParsedType>,
+  InitialValue = NullableUnparsedParsedType | undefined,
+  ParsedType = InitialValue extends undefined
+    ? NullableUnparsedParsedType
+    : ParseFunction extends undefined
+    ? NullableUnparsedParsedType
+    : NonNullable<NullableUnparsedParsedType>
+> = NonNullable<ParsedType> extends string
+  ?
+      | [name: Name, config: Config<Props, false, ParsedType, InitialValue>]
+      | [name: Name]
+  : [name: Name, config: Config<Props, true, ParsedType, InitialValue>]
+
+type Returns<
+  Props extends Record<string, unknown> = Record<string, string>,
+  Name extends keyof Props = keyof Props,
+  NullableUnparsedParsedType extends Props[Name] | undefined =
+    | Props[Name]
+    | undefined,
+  ParseFunction extends
+    | undefined
+    | ((
+        value?: string | string[]
+      ) => NonNullable<NullableUnparsedParsedType>) = (
+    value?: string | string[]
+  ) => NonNullable<NullableUnparsedParsedType>,
+  InitialValue = NullableUnparsedParsedType | undefined,
+  ParsedType = InitialValue extends undefined
+    ? NullableUnparsedParsedType
+    : ParseFunction extends undefined
+    ? NullableUnparsedParsedType
+    : NonNullable<NullableUnparsedParsedType>
+> = readonly [
+  state: ParsedType | InitialValue,
+  setState: (value: ParsedType) => void
+]
+
+export function createParam<
+  Props extends Record<string, unknown> = Record<string, string>
+>() {
+  function useParam<
+    Name extends keyof Props,
+    NullableUnparsedParsedType extends Props[Name] | undefined =
+      | Props[Name]
+      | undefined,
+    ParseFunction extends
+      | undefined
+      | ((
+          value?: string | string[]
+        ) => NonNullable<NullableUnparsedParsedType>) = (
+      value?: string | string[]
+    ) => NonNullable<NullableUnparsedParsedType>,
+    InitialValue = NullableUnparsedParsedType | undefined,
+    ParsedType = InitialValue extends undefined
+      ? NullableUnparsedParsedType
+      : ParseFunction extends undefined
+      ? NullableUnparsedParsedType
+      : NonNullable<NullableUnparsedParsedType>
+  >(
+    ...[name, maybeConfig]: Params<
+      Props,
+      Name,
+      NullableUnparsedParsedType,
+      ParseFunction,
+      InitialValue,
+      ParsedType
+    >
+  ): Returns<
+    Props,
+    Name,
+    NullableUnparsedParsedType,
+    ParseFunction,
+    InitialValue,
+    ParsedType
+  > {
+    const {
+      parse,
+      initial,
+      stringify = (value: ParsedType) => `${value}`,
+      paramsToClearOnSetState,
+    } = maybeConfig || {}
+    const nextRouter = useRouter()
+    const nativeRoute = useRoute()
+
+    const [nativeState, setNativeState] = useState<ParsedType | InitialValue>(
+      () => (nativeRoute?.params as any)?.[name] ?? (initial as InitialValue)
+    )
+
+    const stableStringify = useStableCallback(stringify)
+    const stableParse = useStableCallback(parse)
+    const stableParamsToClear = useStable(paramsToClearOnSetState)
+
+    const initialValue = useRef(initial)
+    const hasSetState = useRef(false)
+
+    const setState = useCallback(
+      (value: ParsedType) => {
+        hasSetState.current = true
+        const { pathname, query } = Router
+        const newQuery = { ...query }
+        if (value != null && (value as any) !== '') {
+          newQuery[name as string] = stableStringify(value)
+        } else {
+          delete newQuery[name as string]
+        }
+
+        if (stableParamsToClear.current) {
+          for (const paramKey of stableParamsToClear.current) {
+            delete newQuery[paramKey as string]
+          }
+        }
+
+        const willChangeExistingParam =
+          query[name as string] && newQuery[name as string]
+
+        const action = willChangeExistingParam ? Router.replace : Router.push
+
+        action(
+          {
+            pathname,
+            query: newQuery,
+          },
+          undefined,
+          {
+            shallow: true,
+          }
+        )
+      },
+      [name, stableStringify, stableParamsToClear]
+    )
+
+    const webParam = nextRouter?.query?.[name as string]
+
+    const state = useMemo<ParsedType>(() => {
+      let state: ParsedType
+      if (webParam === undefined && !hasSetState.current) {
+        state = initialValue.current as any
+      } else if (stableParse) {
+        state = stableParse?.(webParam)
+      } else {
+        state = webParam as any
+      }
+      return state
+    }, [stableParse, webParam])
+
+    if (Platform.OS !== 'web') {
+      return [nativeState, setNativeState]
+    }
+
+    return [state, setState]
+  }
+
+  return {
+    useParam,
+  }
+}
