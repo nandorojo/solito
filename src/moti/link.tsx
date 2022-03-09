2be7713ab427890c71ab4887eea1f4d30bbb7afe
Fernando Rojo <frojo@sas.upenn.edu>
2022-03-09 11:21:43 -0500

    :sparkles: First-class moti integration

---
+++ src/moti/link.tsx
@@ -0,0 +1,16 @@
+import { MotiPressableProps, MotiPressable } from 'moti/interactions'
+import React, { forwardRef } from 'react'
+import type { View } from 'react-native'
+
+import { useLink, UseLinkProps } from '../link'
+
+export type MotiLinkProps = UseLinkProps &
+  Omit<MotiPressableProps, keyof UseLinkProps>
+
+export const MotiLink = forwardRef<View, MotiLinkProps>((props, ref) => {
+  const linkProps = useLink(props)
+
+  return <MotiPressable {...props} {...linkProps} ref={ref} />
+})
+
+MotiLink.displayName = 'MotiLink'
