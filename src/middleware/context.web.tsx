3a86fe6fefd9359386d4d0825477261d4dd63251
Fernando Rojo <frojo@sas.upenn.edu>
2022-04-27 11:19:36 -0400

    Update context.web.tsx

--- src/middleware/context.web.tsx
+++ src/middleware/context.web.tsx
@@ -2,12 +2,14 @@ import { createContext } from 'react'
 
 import { MiddlewareContextType } from './types'
 
+function error() {
+  throw new Error(
+    '[solito] useLinkTo should not be called on Web. Is next/router defined?'
+  )
+}
+
 export const MiddlewareContext = createContext<MiddlewareContextType>({
   useLinkTo() {
-    return function () {
-      throw new Error(
-        '[solito] useLinkTo should not be called on Web. Is next/router defined?'
-      )
-    }
+    return error
   },
 })
