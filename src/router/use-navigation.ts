511b63c58538d386eb0eca200f412494c03c2784
Mark Eggers <mark_eggers@hotmail.com>
2022-05-07 08:20:52 +0200

    :bug: Ensure useRouter back() works with container
    fixes an issue where using back() from useRouter doesn't work from a component instantiated between the NavigationContainer and the Navigators.

--- src/router/use-navigation.ts
+++ src/router/use-navigation.ts
@@ -1,4 +1,15 @@
-import { NavigationContext } from '@react-navigation/core'
+import { NavigationContext, NavigationContainerRefContext } from '@react-navigation/core'
 import { useContext } from 'react'
 
-export const useNavigation = () => useContext(NavigationContext)
+export const useNavigation = () => {
+  const root = useContext(NavigationContainerRefContext)
+  const navigation = useContext(NavigationContext)
+
+  if (navigation === undefined && root === undefined) {
+    throw new Error(
+      "Couldn't find a navigation object. Is your component inside NavigationContainer?"
+    )
+  }
+
+  return navigation !== null && navigation !== void 0 ? navigation : root
+}
