5fd9b0b92d68b6b96c0aa9784fa355d8179eb7e3
Fernando Rojo <frojo@sas.upenn.edu>
2022-05-24 19:33:40 -0500

    📝 pr link

--- src/router/linking-context.ts
+++ src/router/linking-context.ts
@@ -3,7 +3,7 @@
 // THIS IS SUPER UNSAFE
 // WE SHOULD BE USING THE EXPOSED VARIABLE
 // see: https://github.com/react-navigation/react-navigation/discussions/10517
-// PR:
+// PR: https://github.com/react-navigation/react-navigation/pull/10604
 import LinkingContext from '@react-navigation/native/src/LinkingContext'
 
 export { LinkingContext }
