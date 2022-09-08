1bdb9bad1d1fd4c4f1cba50bbd5d7827b46d61d6
Fernando Rojo <frojo@sas.upenn.edu>
2022-09-08 15:28:56 -0400

    ⚡️ sdk 46 & next 12.2.5

--- example-monorepos/blank/apps/next/next.config.js
+++ example-monorepos/blank/apps/next/next.config.js
@@ -1,4 +1,3 @@
-/** @type {import('next').NextConfig} */
 const { withExpo } = require('@expo/next-adapter')
 const withFonts = require('next-fonts')
 const withImages = require('next-images')
@@ -11,12 +10,22 @@ const withTM = require('next-transpile-modules')([
   'app',
 ])
 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // reanimated (and thus, Moti) doesn't work with strict mode currently...
+  // https://github.com/nandorojo/moti/issues/224
+  // https://github.com/necolas/react-native-web/pull/2330
+  // https://github.com/nandorojo/moti/issues/224
+  // once that gets fixed, set this back to true
+  reactStrictMode: false,
+  webpack5: true,
+}
+
 const transform = withPlugins([withTM, withFonts, withImages, withExpo])
 
 module.exports = function (name, { defaultConfig }) {
   return transform(name, {
     ...defaultConfig,
-    webpack5: true,
-    reactStrictMode: true,
+    ...nextConfig,
   })
 }
--- example-monorepos/blank/apps/next/pages/_app.tsx
+++ example-monorepos/blank/apps/next/pages/_app.tsx
@@ -1,8 +1,20 @@
+import 'raf/polyfill'
+
+const fixReanimatedIssue = () => {
+  // FIXME remove this once this reanimated fix gets released
+  // https://github.com/software-mansion/react-native-reanimated/issues/3355
+  if (process.browser) {
+    // @ts-ignore
+    window._frameTimestamp = null
+  }
+}
+
+fixReanimatedIssue()
+
 import { Provider } from 'app/provider'
 import Head from 'next/head'
 import React from 'react'
 import type { SolitoAppProps } from 'solito'
-import 'raf/polyfill'
 
 function MyApp({ Component, pageProps }: SolitoAppProps) {
   return (
