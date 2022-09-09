0c1ea391eb7f96f6214af8b1bebe28770af3fbcb
michaelangrivera <mar@michaelangrivera.com>
2022-09-09 13:41:30 -0400

    remove example repo

--- example-monorepos/nestjs-example/apps/nestjs/src/app.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class AppService {
-  async getHello(): Promise<{ message: string }> {
-    return { message: 'Hello World' };
-  }
-}
