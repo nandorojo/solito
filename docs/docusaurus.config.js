cb2c0e941ff8fe36867ceec339417fe69337fd82
Fernando Rojo <frojo@sas.upenn.edu>
2022-11-25 11:03:18 -0500

    show author, date

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -32,8 +32,8 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl: 'https://github.com/nandorojo/solito/tree/master/docs',
-          showLastUpdateAuthor: false,
-          showLastUpdateTime: false,
+          showLastUpdateAuthor: true,
+          showLastUpdateTime: true,
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
