65d42c1e8ed076b533680153ec90f768f193d0f0
Manish Singh <manish@growthcurve.io>
2022-12-23 10:38:18 +0530

    expo-router plugin in babel

--- example-monorepos/with-expo-router-tailwind/apps/expo/babel.config.js
+++ example-monorepos/with-expo-router-tailwind/apps/expo/babel.config.js
@@ -2,6 +2,11 @@ module.exports = function (api) {
   api.cache(true)
   return {
     presets: [['babel-preset-expo', { jsxRuntime: 'automatic' }]],
-    plugins: ['react-native-reanimated/plugin', 'nativewind/babel'],
+    plugins: [
+      'react-native-reanimated/plugin',
+      // https://expo.github.io/router/docs/intro#configure-the-babel-plugin
+      require.resolve('expo-router/babel'),
+      'nativewind/babel',
+    ],
   }
 }
