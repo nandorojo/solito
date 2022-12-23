db8aed806bc8040d613287dd4493d43aa13c626c
Manish Singh <manish@growthcurve.io>
2022-12-23 09:27:57 +0530

    changing tailwing content filetypes dir

--- example-monorepos/with-expo-router-tailwind/apps/expo/tailwind.config.js
+++ example-monorepos/with-expo-router-tailwind/apps/expo/tailwind.config.js
@@ -6,7 +6,7 @@ const { theme } = require('app/design/tailwind/theme')
  * @type {import('tailwindcss').Config}
  */
 module.exports = {
-  content: ['./App.tsx', '../../packages/**/*.{js,jsx,ts,tsx}'],
+  content: ['./app/**/*.{js,jsx,ts,tsx}', '../../packages/**/*.{js,jsx,ts,tsx}'],
   theme: {
     ...theme,
   },
