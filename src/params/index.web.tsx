91b9bab5c5ad2221f43c37bff97fe6417b93ebdc
Ovidiu Cristescu <55203625+criszz77@users.noreply.github.com>
2023-02-04 22:03:18 +0200

    Refactor createParam to platform specific files

--- src/params/index.web.tsx
+++ src/params/index.web.tsx
@@ -1,7 +1,29 @@
-import { useCallback, useState } from 'react'
+import Router from 'next/router'
+import { useCallback, useEffect, useMemo, useRef } from 'react'
 
-import { useNavigation } from '../router/use-navigation'
-import { useRoute } from './use-route'
+import { useRouter } from './use-router'
+
+function useStable<T>(value: T) {
+  const ref = useRef(value)
+  useEffect(() => {
+    ref.current = value
+  }, [value])
+
+  return ref
+}
+
+function useStableCallback<T extends (...args: any[]) => any>(
+  callback: T | undefined
+): T {
+  const callbackRef = useRef(callback)
+
+  useEffect(() => {
+    callbackRef.current = callback
+  })
+
+  // https://github.com/facebook/react/issues/19240
+  return useMemo(() => ((...args) => callbackRef.current?.(...args)) as T, [])
+}
 
 type Config<
   Props extends Record<string, unknown>,
@@ -107,38 +129,70 @@ export function createParam<
     InitialValue,
     ParsedType
   > {
-    const { initial } = maybeConfig || {}
-    const nativeRoute = useRoute()
-    const nativeNavigation = useNavigation()
-    const nativeStateFromParams = (nativeRoute?.params as any)?.[
-      name
-    ] as ParsedType
+    const {
+      parse = (value?: string | string[]) => value,
+      initial,
+      stringify = (value: ParsedType) => `${value}`,
+      paramsToClearOnSetState,
+    } = maybeConfig || {}
+    const nextRouter = useRouter()
 
-    const [nativeStateFromReact, setNativeStateFromReact] = useState<
-      ParsedType | InitialValue
-    >(() => nativeStateFromParams ?? (initial as InitialValue))
+    const stableStringify = useStableCallback(stringify)
+    const stableParse = useStableCallback(parse)
+    const stableParamsToClear = useStable(paramsToClearOnSetState)
 
-    const setNativeStateFromParams = useCallback((value: ParsedType) => {
-      nativeNavigation?.setParams({
-        [name]: value,
-      })
-    }, [])
+    const initialValue = useRef(initial)
+    const hasSetState = useRef(false)
 
-    const nativeState = nativeRoute
-      ? nativeStateFromParams
-      : nativeStateFromReact
-    const setNativeState = nativeRoute
-      ? setNativeStateFromParams
-      : setNativeStateFromReact
+    const setState = useCallback(
+      (value: ParsedType) => {
+        hasSetState.current = true
+        const { pathname, query } = Router
+        const newQuery = { ...query }
+        if (value != null && (value as any) !== '') {
+          newQuery[name as string] = stableStringify(value)
+        } else {
+          delete newQuery[name as string]
+        }
 
-    if (!nativeRoute) {
-      console.error(
-        `[solito] useParam('${
-          name as string
-        }') called when there is no React Navigation route available. In a future version, this will throw an error. Please fix this by only calling useParam() inside of a React Navigation route. For now, Solito will fallback to using React state.`
-      )
-    }
-    return [nativeState, setNativeState]
+        if (stableParamsToClear.current) {
+          for (const paramKey of stableParamsToClear.current) {
+            delete newQuery[paramKey as string]
+          }
+        }
+
+        const willChangeExistingParam =
+          query[name as string] && newQuery[name as string]
+
+        const action = willChangeExistingParam ? Router.replace : Router.push
+
+        action(
+          {
+            pathname,
+            query: newQuery,
+          },
+          undefined,
+          {
+            shallow: true,
+          }
+        )
+      },
+      [name, stableStringify, stableParamsToClear]
+    )
+
+    const webParam = nextRouter?.query?.[name as string]
+
+    const state = useMemo<ParsedType>(() => {
+      let state: ParsedType
+      if (webParam === undefined && !hasSetState.current) {
+        state = initialValue.current as any
+      } else {
+        state = stableParse(webParam) as ParsedType
+      }
+      return state
+    }, [stableParse, webParam])
+
+    return [state, setState]
   }
 
   type UpdateOptions = {
@@ -151,13 +205,33 @@ export function createParam<
     props: Partial<Props>,
     options?: UpdateOptions
   ) => void {
-    const nativeNavigation = useNavigation()
+    const setWebState = useCallback(
+      (value: Partial<Props>, options?: UpdateOptions) => {
+        const { pathname, query } = Router
+        const newQuery = { ...query, ...value }
+        for (const key in value) {
+          if (value[key] == null || value[key] === '') {
+            delete newQuery[key]
+          }
+        }
 
-    const setNativeStateFromParams = useCallback((value: Partial<Props>) => {
-      nativeNavigation?.setParams(value)
-    }, [])
+        const action = options?.web?.replace ? Router.replace : Router.push
 
-    return setNativeStateFromParams
+        action(
+          {
+            pathname,
+            query: newQuery,
+          },
+          undefined,
+          {
+            shallow: true,
+          }
+        )
+      },
+      []
+    )
+
+    return setWebState
   }
 
   return {
