56eb053a89f17deeaa7008cd96e07a2e1a82364e
Fernando Rojo <frojo@sas.upenn.edu>
2023-03-17 20:13:59 -0600

    document

--- example-monorepos/with-tailwind/apps/next/pages/_document.tsx
+++ example-monorepos/with-tailwind/apps/next/pages/_document.tsx
@@ -1,66 +1,25 @@
-// Based on https://github.com/zeit/next.js/tree/canary/examples/with-react-native-web
-// and https://github.com/expo/expo-cli/blob/main/packages/webpack-config/web-default/index.html
-import NextDocument, { Head, Html, Main, NextScript } from 'next/document'
-import * as React from 'react'
-// @ts-ignore
-import { AppRegistry } from 'react-native-web'
+import React from 'react'
+import { AppRegistry } from 'react-native'
 
-export const style = `
-/**
- * Building on the RNWeb reset:
- * https://github.com/necolas/react-native-web/blob/master/packages/react-native-web/src/exports/StyleSheet/initialRules.js
- */
-html, body, #__next {
-  width: 100%;
-  /* To smooth any scrolling behavior */
-  -webkit-overflow-scrolling: touch;
-  margin: 0px;
-  padding: 0px;
-  /* Allows content to fill the viewport and go beyond the bottom */
-  min-height: 100%;
-}
-#__next {
-  flex-shrink: 0;
-  flex-basis: auto;
-  flex-direction: column;
-  flex-grow: 1;
-  display: flex;
-  flex: 1;
-}
-html {
-  scroll-behavior: smooth;
-  /* Prevent text size change on orientation change https://gist.github.com/tfausak/2222823#file-ios-8-web-app-html-L138 */
-  -webkit-text-size-adjust: 100%;
-  height: 100%;
-}
-body {
-  display: flex;
-  /* Allows you to scroll below the viewport; default value is visible */
-  overflow-y: auto;
-  overscroll-behavior-y: none;
-  text-rendering: optimizeLegibility;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  -ms-overflow-style: scrollbar;
-}
-`
+import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
+import type { DocumentContext } from 'next/document'
 
-export async function getInitialProps({ renderPage }: any) {
-  AppRegistry.registerComponent('Main', () => Main)
-  const { getStyleElement } = AppRegistry.getApplication('Main')
-  const page = await renderPage()
-  const styles = [
-    <style key="style-reset" dangerouslySetInnerHTML={{ __html: style }} />,
-    getStyleElement(),
-  ]
-  return { ...page, styles: React.Children.toArray(styles) }
-}
+class Document extends NextDocument {
+  static async getInitialProps(ctx: DocumentContext) {
+    AppRegistry.registerComponent('Main', () => Main)
+    // @ts-ignore
+    const { getStyleElement } = AppRegistry.getApplication('Main')
+    const styles = [getStyleElement()]
 
-export class Document extends NextDocument {
+    const initialProps = await NextDocument.getInitialProps(ctx)
+    return { ...initialProps, styles: React.Children.toArray(styles) }
+  }
+
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
+          <meta charSet="UTF-8" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         </Head>
         <body>
@@ -71,7 +30,5 @@ export class Document extends NextDocument {
     )
   }
 }
-
-Document.getInitialProps = getInitialProps
 
 export default Document
