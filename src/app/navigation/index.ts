5ef1f0725f797d8260dfd0879846537c93202bf7
Fernando Rojo <frojo@sas.upenn.edu>
2023-06-01 13:17:41 -0400

    oops

--- src/app/navigation/index.ts
+++ src/app/navigation/index.ts
@@ -3,20 +3,3 @@ export * from './use-params'
 export * from './use-search-params'
 export * from './use-pathname'
 export { default as useUpdateSearchParams } from './use-update-search-params'
-import useUpdateSearchParams from './use-update-search-params'
-
-type Params = {
-  id: string
-}
-
-export function App() {
-  const updateParams = useUpdateSearchParams<Params>()
-
-  const onPress = () => {
-    updateParams({ id: '123' })
-
-    // by default, router.replace is called on Web if you're updating an existing param
-    // to override this, see the  webBehavior property
-    updateParams({ id: '123' }, { webBehavior: 'push' })
-  }
-}
