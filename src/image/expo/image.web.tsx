3e31a4364793700c640565b5d36d30cd59f5aeff
Fernando Rojo <frojo@sas.upenn.edu>
2023-10-19 11:42:22 -0400

    forwardRef to NextImage

--- src/image/expo/image.web.tsx
+++ src/image/expo/image.web.tsx
@@ -11,26 +11,25 @@ import { Image, ImageResizeMode, StyleSheet } from 'react-native'
 // @ts-expect-error missing types
 import { unstable_createElement } from 'react-native-web'
 
-// import useElementLayout from 'react-native-web/dist/modules/useElementLayout'
-// import { mergeRefs } from '../../helpers/merge-refs'
 import { useSolitoImageContext } from '../context'
 import { SolitoImageProps } from '../image.types'
 
 const SolitoImage = forwardRef<Image, SolitoImageProps>(function SolitoImage(
-  { contentFit, resizeMode = 'contain', fill, style, onLayout, contentPosition, ...props },
+  {
+    contentFit,
+    resizeMode = 'contain',
+    fill,
+    style,
+    onLayout,
+    contentPosition,
+    ...props
+  },
   ref
 ) {
   const { loader } = useSolitoImageContext()
-  // add back onLayout when this is solved: https://github.com/vercel/next.js/discussions/43267
-  // const localRef = useRef<Image>(null)
-  // useElementLayout(
-  //   // https://github.com/necolas/react-native-web/blob/master/packages/react-native-web/src/exports/View/index.js#L88
-  //   localRef,
-  //   onLayout
-  // )
   return unstable_createElement(NextImage, {
     ...props,
-    // ref: mergeRefs([ref, localRef]),
+    ref,
     loader: props.loader ?? loader,
     fill,
     style: [
