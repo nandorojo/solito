d36a6450d5657416eed68120121428eee9628a23
Obed Nuertey <onuertey1997@gmail.com>
2024-10-07 14:26:35 +0000

    🔧 Inject environment variables
    Updated ./apps/expo/babel.config.js file to inject environmental variables into next and expo packages

--- example-monorepos/with-custom-font/apps/expo/babel.config.js
+++ example-monorepos/with-custom-font/apps/expo/babel.config.js
@@ -1,7 +1,24 @@
 module.exports = function (api) {
   api.cache(true)
+
+  // Get the current environment or default to 'development'
+  const APP_ENV = process.env.APP_ENV || 'development';
+
+  // Set the path based on the environment
+  const envPath = `.env.${APP_ENV}`;
+  
   return {
     presets: [['babel-preset-expo', { jsxRuntime: 'automatic' }]],
-    plugins: ['react-native-reanimated/plugin'],
+    plugins: [
+      [
+        'module:react-native-dotenv',
+        {
+          envName: 'APP_ENV',
+          moduleName: '@env',
+          path: envPath
+        },
+      ],
+      'react-native-reanimated/plugin'
+    ],
   }
 }
