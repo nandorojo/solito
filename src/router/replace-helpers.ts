7d9c4eade32d0d7619b6d30d0b8cc6aa47b1cac0
Troy Shu <tmshu1@gmail.com>
2024-11-08 20:23:52 -0500

    fix LinkingContext import for react navigation 7

--- src/router/replace-helpers.ts
+++ src/router/replace-helpers.ts
@@ -9,6 +9,6 @@ import {
 // WE SHOULD BE USING THE EXPOSED VARIABLE
 // see: https://github.com/react-navigation/react-navigation/discussions/10517
 // PR: https://github.com/react-navigation/react-navigation/pull/10604
-import LinkingContext from '@react-navigation/native/src/LinkingContext'
+import { LinkingContext } from '@react-navigation/native'
 
 export { LinkingContext, StackActions, getStateFromPath, getActionFromState }
