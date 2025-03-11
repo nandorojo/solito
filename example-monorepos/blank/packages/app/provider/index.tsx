4ab4363adcadf9fb51c1c0c8fa80a02000673dcd
Fernando Rojo <fernando.rojo@vercel.com>
2025-03-11 16:17:00 -0400

    native "works"...

--- example-monorepos/blank/packages/app/provider/index.tsx
+++ example-monorepos/blank/packages/app/provider/index.tsx
@@ -1,5 +1,10 @@
+import { SafeArea } from 'app/provider/safe-area'
 import { NavigationProvider } from './navigation'
 
 export function Provider({ children }: { children: React.ReactNode }) {
-  return <NavigationProvider>{children}</NavigationProvider>
+  return (
+    <SafeArea>
+      <NavigationProvider>{children}</NavigationProvider>
+    </SafeArea>
+  )
 }
