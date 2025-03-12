96a60746969a970ba146e7501c4609c6c9f92859
Fernando Rojo <fernando.rojo@vercel.com>
2025-03-12 08:54:51 -0400

    repro

--- example-monorepos/blank/apps/next/next.config.js
+++ example-monorepos/blank/apps/next/next.config.js
@@ -1,16 +1,3 @@
-const resolveAlias = {
-  // Alias direct react-native imports to react-native-web
-  'react-native': 'react-native-web',
-  'react-native$': 'react-native-web',
-  // Alias internal react-native modules to react-native-web
-  'react-native/Libraries/EventEmitter/RCTDeviceEventEmitter$':
-    'react-native-web/dist/vendor/react-native/NativeEventEmitter/RCTDeviceEventEmitter',
-  'react-native/Libraries/vendor/emitter/EventEmitter$':
-    'react-native-web/dist/vendor/react-native/emitter/EventEmitter',
-  'react-native/Libraries/EventEmitter/NativeEventEmitter$':
-    'react-native-web/dist/vendor/react-native/NativeEventEmitter',
-}
-
 const resolveExtensions = ['.web.js', '.web.jsx', '.web.ts', '.web.tsx']
 
 /**
