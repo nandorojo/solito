89927432cf03eb9818000c03b4f3aea161ea7254
Fernando Rojo <fernando.rojo@vercel.com>
2025-10-21 15:40:22 -0700

    notes

--- src/app/navigation/use-search-params.ts
+++ src/app/navigation/use-search-params.ts
@@ -26,29 +26,23 @@ export function useSearchParams<Type extends Record<string, string>>() {
 
   const params = route?.params as Type | undefined
 
-  if (__DEV__) {
-    const nonStringParamValues = Object.entries(params || {})
-      .map(([key, value]) => {
-        if (typeof value !== 'string') {
-          return `${key}: ${JSON.stringify(value)}`
-        }
-        return undefined
-      })
-      .filter(Boolean)
-
-    if (nonStringParamValues.length) {
-      throw new Error(`[useSearchParams][solito] Error found in the "${
-        route?.name ?? 'unknown'
-      }" screen (path ${
-        route?.path ?? 'unknown'
-      }). You used non-string parameters for the following params:
-      
-${nonStringParamValues.join('\n')}
-
-Due to constraints from Next.js, this is not valid in Solito.  Please refactor your code to use strings for screen parameters.
-`)
-    }
-  }
-
-  return useMemo(() => params && new URLSearchParams(params), [params])
+  return useMemo(
+    () =>
+      params &&
+      new URLSearchParams(
+        Object.entries(params).map(([key, value]) => {
+          if (typeof value === 'string') {
+            return [key, value]
+          }
+          if (typeof value === 'number') {
+            return [key, Number(value).toString()]
+          }
+          if (typeof value === 'boolean') {
+            return [key, value]
+          }
+          return []
+        })
+      ),
+    [params]
+  )
 }
